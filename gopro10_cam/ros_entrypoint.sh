df1c81f1924a17d96517d153430a297cf4313e03
katafoxi <katafoxi@gmail.com>
2024-08-13 13:08:52 +0300

    reorganisation folder structure

---
+++ gopro10_cam/ros_entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+    set -e
+
+    # setup ros environment
+    if [ -z "${SETUP}" ]; then
+        # basic ros environment
+        source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+    else
+       #from environment variable; should be a absolute path to the appropriate workspaces's setup.bash
+        #source $SETUP
+        source "/opt/ros/overlay_ws/devel/setup.bash"
+    fi
+source "/opt/ros/overlay_ws/devel/setup.bash"
+roslaunch rovio rovio_rosbag_testnode.launch
+#rosbag play --delay 10 -q  /opt/ros/rosbag_testdata/MH_01_easy.bag
+#    roslaunch rovio rovio_node.launch
+    exec "$@"
