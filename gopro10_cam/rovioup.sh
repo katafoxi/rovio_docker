dfde863109502d4c5ef865c7d7fd5f9965280c1f
Maksim Lapin <katafoxi@gmail.com>
2024-08-22 22:42:26 +0300

    add chmod 666 to i2c device

--- gopro10_cam/rovioup.sh
+++ gopro10_cam/rovioup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo chmod 666 /dev/i2c-1    # https://answers.ros.org/question/397084/i2c-device-open-failed/
 XAUTH=./.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
